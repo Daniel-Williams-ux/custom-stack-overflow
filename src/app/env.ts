13a7bdb5ec4de20c23b264ec6314f11217bc230f
root <root@INSPIRON-5567.localdomain>
2025-02-20 13:04:52 +0100

    Add TypeScript type definitions for env configuration

---
+++ src/app/env.ts
@@ -0,0 +1,26 @@
+interface AppwriteConfig {
+    endpoint: string;
+    projectId: string;
+    apikey: string;
+}
+
+const env: { appwrite: AppwriteConfig } = {
+    appwrite: {
+        endpoint: String(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT),
+        projectId: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
+        apikey: String(process.env.APPWRITE_API_KEY),
+    }
+}
+
+export default env;
+
+
+
+// const env = {
+//     appwrite: {
+//         endpoint: String(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT),
+//         projectId: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
+//         apikey: String(process.env.APPWRITE_API_KEY),
+//     }
+// }
+// export default env
