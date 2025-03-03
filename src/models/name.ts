99643c6a02486bdaa184fd7f5214908756edf641
root <root@INSPIRON-5567.localdomain>
2025-03-03 11:08:29 +0100

    create and configure question collection in appwrite database

--- src/models/name.ts
+++ src/models/name.ts
@@ -1,6 +1,6 @@
 export const db = "main-stackflow"
 export const questionCollection = "questions"
-export const anserCollection = "answers"
+export const answerCollection = "answers";
 export const commentCollection = "comments"
 export const voteCollection = "votes"
-export const questionAttachmentBucket = "question-attachement"
+export const questionAttachmentBucket = "question-attachment";
